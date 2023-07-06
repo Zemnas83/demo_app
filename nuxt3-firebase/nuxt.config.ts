3fd066bb1fb9b5f12784e9139c15c7034aeac8bd
Zemnas83 <zayanmatin@gmail.com>
2023-07-06 11:31:47 -0400

    initial commit

---
+++ nuxt3-firebase/nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  runtimeConfig: {
+    FIREBASE_API_KEY: "AIzaSyCKpdXQNit0YUj0cOIGbJN7uQIAGDwk-6E",
+  
+  public: {
+    FIREBASE_API_KEY: "AIzaSyCKpdXQNit0YUj0cOIGbJN7uQIAGDwk-6E",
+  }
+  }
+})
---
+++ nuxt3-firebase/plugins/firebaseAuth.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+export default defineNuxtPlugin ((nuxtApp) => {
+
+    const config = useRuntimeConfig();
+    
+    const firebaseConfig = {
+        apiKey: "AIzaSyCKpdXQNit0YUj0cOIGbJN7uQIAGDwk-6E",
+    }
+    
+    const app = initializeApp(firebaseConfig);
+
+    console.log(app);
+});
